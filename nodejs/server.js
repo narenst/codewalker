a7959db17c236dd068783352db5b75e0c3ce50c1
Naren Thiagarajan <naren@locationlabs.com>
2012-05-11 07:49:11 -0700

    heroku integration

--- nodejs/server.js
+++ nodejs/server.js
@@ -52,7 +52,7 @@ io.sockets.on('connection', function (socket) {
       master = socket;
       broadcastMasterStatus();
    }
-
+   
    function clearMaster() {
       master = null;
       masterSelected = false;
